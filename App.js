caf09ab2bce006d0f28b0a5cc0c9d20038243024
rebekahshaw92 <rebekahshaw92@yahoo.co.uk>
2020-08-12 10:26:08 +0100

    Updated local push notifications

--- App.js
+++ App.js
@@ -7,57 +7,45 @@
  */
 
 import React from 'react';
-import {Button, View} from 'react-native';
+//import {TouchableOpacity, View, Text} from 'react-native';
 import {Notifications} from 'react-native-notifications';
 
 import HomeStack from './app/nav';
 
-
-// let someLocalNotification = Notifications.postLocalNotification({
-//   body: "Local notification!",
-//   title: "Local Notification Title",
-//   sound: "chime.aiff",
-//   category: "SOME_CATEGORY",
-//   userInfo: { }
-// });
-
-// Notifications.cancelLocalNotification(someLocalNotification);
-
- export default class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     Notifications.registerRemoteNotifications();
 
     Notifications.events().registerNotificationReceivedForeground((notification: Notification, completion) => {
       console.log(`Notification received in foreground: ${notification.title} : ${notification.body}`);
-      completion({alert: false, sound: false, badge: false});
-    });
+      completion({alert: true, sound: false, badge: true});
+      },
+    );
 
     Notifications.events().registerNotificationOpened((notification: Notification, completion) => {
       console.log(`Notification opened: ${notification.payload}`);
       completion();
-    });
+      },
+    );
+  }
+}
 
-    someLocalNotification = Notifications.postLocalNotification({
- body: "Local notification!",
-  title: "Local Notification Title",
-  sound: "chime.aiff",
-  category: "SOME_CATEGORY",
-   userInfo: { }
+  someLocalNotification = Notifications.postLocalNotification({
+    body: 'Local notification!',
+    title: 'Local Notification!',
+    //sound: "chime.aiff",
+    category: 'SOME_CATEGORY',
+    userInfo: {},
  });
 
-   render() {
+render() {
     return (
       <>
- 
-    <HomeStack />
-    <View>
-    <Button  
-             onPress={this.someLocalNotification}  
-             title="Press Me"  
-                    /> 
-   </View>
-      </>
+      <HomeStack />
+   </>
     )
    }   
-}
+  }
+
+
